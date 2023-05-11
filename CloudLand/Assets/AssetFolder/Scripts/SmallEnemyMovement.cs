08c63ddf482b0933a0320d4062625383833f7c06
LARRYKEMMO <larryloickemmotejiokem@gmail.com>
2023-05-11 15:21:28 -0300

    Commit Experimental scene

---
+++ CloudLand/Assets/AssetFolder/Scripts/SmallEnemyMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallEnemyMovement : MonoBehaviour
+{
+    public GameObject eyes;
+    private bool FacingL = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
